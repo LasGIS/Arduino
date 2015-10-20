3aa409cecd1487644eba2a25fa74b745b4f8958d
laskin <laskin@ba01d2ab-96a8-42fd-9c88-743d4ad6a346>
2015-10-20 16:23:09 +0000

    Arduino::вывод часов.

--- branches/arduino/TimePanel/TimePanel.ino
+++ branches/arduino/TimePanel/TimePanel.ino
@@ -10,6 +10,41 @@ void setup() {
 }
 
 void loop() {
+  panelValue = timeToString();
+  delay(100);
+}
+
+String timeToString() {
+  unsigned long time = millis();
+  time /= 1000;
+  int sec = time % 60;
+  time /= 60;
+  int min = time % 60;
+  time /= 60;
+  int hour = time;
+  /*
+  Serial.print("hour = ");
+  Serial.print(hour);
+  Serial.print("; min = ");
+  Serial.print(min);
+  Serial.print("; sec = ");
+  Serial.print(sec);
+  Serial.println(";");
+  */
+  if (hour > 0) {
+    return toTwo(hour) + "." + toTwo(min);
+  } else {
+    return toTwo(min) + "." + toTwo(sec);
+  }
+}
+
+String toTwo(int val) {
+  String str = "00";
+  str = str + val;
+  return str.substring(str.length() - 2);
+}
+
+/*
   for (double val = -11.0; val < 11.0; val += .01) {
     panelValue = String(val, 2);
     Serial.print(val);
@@ -18,5 +53,5 @@ void loop() {
   }
   panelValue = "B.0.b.a.";
   delay(10000);
-}
+*/
 
