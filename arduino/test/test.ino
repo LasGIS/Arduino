9b460b3d0852d044cbb5e8d1f9a756ed6b9cd927
LasGIS <laskin@yandex.ru>
2015-11-16 22:52:22 +0600

    Arduino::коррекция библиотек.

---
+++ arduino/test/test.ino
@@ -0,0 +1,24 @@
+#include <IrTest2.h>
+
+// указываем пин для ИК датчика 
+IrTest2 test(2);
+
+void setup() {
+  Serial.begin(9600);
+  test.start();
+}
+
+void loop() {
+  delay(100);
+}
+
+void serialEvent() {
+  char buf[10];
+  int cnt = Serial.readBytes(buf, 10) - 1;
+  if (cnt >= 0 && cnt < 10) {
+    buf[cnt] = 0;
+  }
+  Serial.println(String("[") + buf + "]");
+  //test.print();
+  delay(1000);
+}
