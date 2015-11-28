14b35998c233d3e733369955e305985fcdd24e5f
LasGIS <laskin@yandex.ru>
2015-11-29 01:55:07 +0600

    Arduino::Коррекция скотч LcdPanel. Делаем редактирование времени.

--- arduino/LcdPanel/RealTime.ino
+++ arduino/LcdPanel/RealTime.ino
@@ -5,22 +5,23 @@ public:
   uint8_t row;
   uint8_t col;
   uint8_t len;
-  byte buf[4];
+  uint16_t maxVal;
+  uint16_t val;
 };
 
 RTField RTFields[RT_MAX_FIELDS + 1] {
-  {0, 0, 3, {'s', 'a', 't', '0'}},
-  {0, 4, 4, {'2', '0', '1', '5'}},
-  {0, 9, 2, {'0', '0', '0', '0'}},
-  {0, 12, 2, {'0', '0', '0', '0'}},
-  {1, 7, 2, {'0', '0', '0', '0'}},
-  {1, 10, 2, {'0', '0', '0', '0'}},
-  {1, 13, 2, {'0', '0', '0', '0'}}
+  {0, 0, 1, 7, 1},
+  {0, 4, 4, 9999, 2015},
+  {0, 9, 2, 12, 1},
+  {0, 12, 2, 31, 1},
+  {1, 7, 2, 23, 0},
+  {1, 10, 2, 59, 0},
+  {1, 13, 2, 59, 0}
 };
 
 String dayAsString(const Time::Day day) {
   switch (day) {
-    case Time::kSunday: { return "Sun"; }
+    case Time::kSunday: return "Sun";
     case Time::kMonday: return "Mon";
     case Time::kTuesday: return "Tue";
     case Time::kWednesday: return "Wed";
@@ -101,10 +102,12 @@ LPMode editTime(char key) {
     showField(nField, nPosit);
     break;
   case '+':
-    break;
   case '-':
+    setValue(nField, nPosit, key);
+    showField(nField, nPosit);
     break;
   case 'p': // записываем и выходим
+    rtc.time(Fields2Time());
     lcd.noCursor();
     lcd.noBlink();
     return show;
@@ -117,11 +120,26 @@ LPMode editTime(char key) {
 }
 
 void Time2Fields(Time tm) {
-  
+  RTFields[0].val = tm.day;
+  RTFields[1].val = tm.yr;
+  RTFields[2].val = tm.mon;
+  RTFields[3].val = tm.date;
+
+  RTFields[4].val = tm.hr;
+  RTFields[5].val = tm.min;
+  RTFields[6].val = tm.sec;
 }
 
 Time Fields2Time() {
   Time tm(2099, 1, 1, 0, 0, 0, Time::kSunday);
+  tm.day = (Time::Day) RTFields[0].val;
+  tm.yr = RTFields[1].val;
+  tm.mon = RTFields[2].val;
+  tm.date = RTFields[3].val;
+
+  tm.hr = RTFields[4].val;
+  tm.min = RTFields[5].val;
+  tm.sec = RTFields[6].val;
   return tm;
 }
 
@@ -130,15 +148,57 @@ Time Fields2Time() {
  */
 void showField(int nField, int nPosit) {
   RTField field = RTFields[nField];
+  char buf[5];
   lcd.setCursor(field.col, field.row);
-  for (int i = 0; i < field.len; i++) {
-    lcd.write(field.buf[i]);
+  if (nField == 0) {
+    lcd.print(dayAsString((Time::Day) field.val));
+  } else {
+    int val = field.val;
+    buf[field.len] = 0;
+    for (int i = field.len - 1; i >= 0; i--) {
+      buf[i] = '0' + val % 10;
+      val = val / 10;
+    }
+    lcd.print(buf);
   }
   lcd.setCursor(field.col + nPosit, field.row);
 }
 
-void setValue(int nField, int nPosit, char val) {
-  RTFields[nField].buf[nPosit] = val;
+/**
+ * устанавливаем значение поля
+ */
+void setValue(int nField, int nPosit, char key) {
+  RTField field = RTFields[nField];
+  byte buf[5];
+  uint16_t val = field.val;
+  for (int i = field.len - 1; i >= 0; i--) {
+    buf[i] = val % 10;
+    val = val / 10;
+  }
+
+  if (key >= '0' && key <= '9') {
+    buf[nPosit] = key - '0';
+  } else switch (key) {
+    case '+':
+    buf[nPosit]++;
+    break;
+    case '-':
+    buf[nPosit]--;
+    break;
+  }
+
+  val = 0;
+  for (int i = 0; i < field.len; i++) {
+    val *= 10;
+    val += buf[i];
+  }
+
+  if (val > field.maxVal) {
+    val = field.maxVal;
+  }
+  int minVal = (nField < 4 ? 1 : 0);
+  if (val < minVal) {
+    val = minVal;
+  }
+  RTFields[nField].val = val;
 }
-
-
