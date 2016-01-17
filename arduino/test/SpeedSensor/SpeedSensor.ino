b2b98f0051e312f1f0cc651aa57374b927983310
LasGIS <laskin@yandex.ru>
2016-01-17 13:31:43 +0600

    Arduino::добавил тест SpeedSensor.

---
+++ arduino/test/SpeedSensor/SpeedSensor.ino
@@ -0,0 +1,26 @@
+/*
+ * счётчик скорости
+ */
+
+// пин для аналогового счётчика скорости
+const int analogSpeedPin = A0;
+
+// пин для цифрового счётчика скорости
+const int speedCountPin = 2;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(speedCountPin, INPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  int anal = analogRead(analogSpeedPin);
+  int digit = digitalRead(speedCountPin);
+  Serial.print("digit = ");
+  Serial.print(digit, BIN);
+  Serial.print("; anal = ");
+  Serial.println(anal, DEC);
+  delay(1000);
+}
+
