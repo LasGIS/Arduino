196e9c0ff7aeea7cc1017abe07dab3d7cdb3cfa8
LasGIS <laskin@yandex.ru>
2016-01-17 14:37:56 +0600

    Arduino::Тест SpeedSensor.

--- arduino/test/SpeedSensor/SpeedSensor.ino
+++ arduino/test/SpeedSensor/SpeedSensor.ino
@@ -6,21 +6,46 @@
 const int analogSpeedPin = A0;
 
 // пин для цифрового счётчика скорости
-const int speedCountPin = 2;
+const int speedometrPin = 2;
+
+static int count = 0;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(speedCountPin, INPUT);
+  pinMode(speedometrPin, INPUT);
+  //прерывание спидометра по фронту импульса
+  attachInterrupt(
+    digitalPinToInterrupt(speedometrPin),
+    speedometr, CHANGE
+  );
 }
 
 // the loop function runs over and over again forever
 void loop() {
+  Serial.print("count = ");
+  Serial.println(count, DEC);
+  delay(1000);
+}
+
+/** 
+ * измеряем частоту на входе спидометра по прерыванию
+ */
+void speedometr() {
+  static int lastVal = 0;
+  int val = digitalRead(speedometrPin);
+  if (val != lastVal) {
+    count++;
+    lastVal = val;
+  }
+/*  
   int anal = analogRead(analogSpeedPin);
-  int digit = digitalRead(speedCountPin);
+  int digit = digitalRead(speedometrPin);
   Serial.print("digit = ");
   Serial.print(digit, BIN);
   Serial.print("; anal = ");
   Serial.println(anal, DEC);
-  delay(1000);
+*/  
+  delay(10);
 }
+
 
