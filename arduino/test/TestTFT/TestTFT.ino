1a8c33e9ed01c385caf55f9b61212ca4b6ed0554
LasGIS <laskin@yandex.ru>
2016-01-23 01:25:12 +0600

    Arduino::Тест TestTFT.

--- arduino/test/TestTFT/TestTFT.ino
+++ arduino/test/TestTFT/TestTFT.ino
@@ -21,20 +21,45 @@ void setup() {
   TFTscreen.begin();
 
   // set the background to white
-  TFTscreen.background(255, 255, 255);
+  TFTscreen.background(0, 0, 0);
 
+  drawBox();
 }
 
 void loop() {
-  static int xMin = 0;
-  static int yMin = 0;
-  static int xMax = TFTscreen.height() - 1;
-  static int yMax = TFTscreen.width() - 1;
+  delay(10);
+}
+
+void drawBox() {
+  int xMin = 3;
+  int yMin = 2;
+  int xMax = TFTscreen.height();
+  int yMax = TFTscreen.width()-2;
+  Serial.print("x0 = ");
+  Serial.print(xMin);
+  Serial.print("; y0 = ");
+  Serial.print(yMin);
+  Serial.print("; xm = ");
+  Serial.print(xMax);
+  Serial.print("; ym = ");
+  Serial.print(yMax);
+  Serial.print("; xlen(width) = ");
+  Serial.print(xMax - xMin);
+  Serial.print("; ylen(height) = ");
+  Serial.print(yMax - yMin);
+  Serial.println(";");
+  
+  TFTscreen.stroke(255, 255, 255);
+  TFTscreen.line(1, 15, 15, 1);
 
   // draw a line in a nice color
+  TFTscreen.stroke(250, 0, 0);
+  TFTscreen.line(xMin, yMin, xMax, yMin);
+  TFTscreen.line(xMax, yMin, xMax, yMax);
+  TFTscreen.line(xMax, yMax, xMin, yMax);
+  TFTscreen.line(xMin, yMax, xMin, yMin);
+
   TFTscreen.stroke(250, 180, 10);
   TFTscreen.line(xMin, yMin, xMax, yMax);
   TFTscreen.line(xMin, yMax, xMax, yMin);
-
-  delay(160);
 }
