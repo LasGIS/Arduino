d0cda082c4e9491cb266b15ebcd33a6eaee60f8d
LasGIS <laskin@yandex.ru>
2016-02-12 00:30:25 +0600

    Arduino::Инфракрасный датчик test.

---
+++ arduino/test/IRDistanse/IRDistanse.ino
@@ -0,0 +1,19 @@
+/**
+ * проверка лазера
+*/
+#define pinL1 3
+#define pinIR A0
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(pinL1, OUTPUT);
+  pinMode(pinIR, INPUT);
+}
+
+void loop() {
+  int L1 = digitalRead(pinIR);
+  digitalWrite(pinL1, L1);
+  Serial.println(L1);
+  delay(100);
+}
+
