eee61ffa4f42d47d0fdd2cadf2790d54ea0d1ecb
LasGIS <laskin@yandex.ru>
2016-03-23 01:43:59 +0700

    VoltInternal::Added

---
+++ arduino/VoltInternal/VoltInternal.ino
@@ -0,0 +1,15 @@
+static double rate = 3500. * 1.0847 / (500. * 1023.);
+
+void setup() {
+  Serial.begin(9600);
+  analogReference(INTERNAL);
+}
+
+void loop() {
+  int val = analogRead(A0);
+  Serial.print("analogRead() = ");
+  Serial.print(val);
+  Serial.print("; V = ");
+  Serial.println(val * rate);
+  delay(1000);
+}
