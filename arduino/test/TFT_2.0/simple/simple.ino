b27e46f47ea9eb8a2ea9b670021335da961454d9
LasGIS <laskin@yandex.ru>
2017-08-24 23:35:27 +0600

    Arduino::Lcd 2.0::Добавил библиотеки. .

---
+++ arduino/test/TFT_2.0/simple/simple.ino
@@ -0,0 +1,28 @@
+// Include application, user and local libraries
+#include "SPI.h"
+#include "TFT_22_ILI9225.h"
+
+#define TFT_RST 8
+#define TFT_RS  9
+#define TFT_CS  10  // SS
+#define TFT_SDI 11  // MOSI
+#define TFT_CLK 13  // SCK
+#define TFT_LED 3   // 0 if wired to +5V directly
+
+// Use hardware SPI (faster - on Uno: 13-SCK, 12-MISO, 11-MOSI)
+TFT_22_ILI9225 tft = TFT_22_ILI9225(TFT_RST, TFT_RS, TFT_CS, TFT_LED);
+// Use software SPI (slower)
+//TFT_22_ILI9225 tft = TFT_22_ILI9225(TFT_RST, TFT_RS, TFT_CS, TFT_SDI, TFT_CLK, TFT_LED);
+
+void setup() {
+  tft.begin();
+  tft.setFont(Terminal6x8);
+  Serial.begin(9600);
+}
+
+void loop() {
+  tft.clear();
+  tft.drawRectangle(0, 0, tft.maxX() - 1, tft.maxY() - 1, COLOR_WHITE);
+  tft.drawText(10, 10, "hello!");
+  delay(1000);
+}
