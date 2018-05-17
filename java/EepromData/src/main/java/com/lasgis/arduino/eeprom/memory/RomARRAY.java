36300e617fa5d2a46d8ca08af267323f1387361c
LasGIS <laskin@yandex.ru>
2018-05-17 20:03:55 +0600

    Eeprom Data: Add xml definition

--- java/EepromData/src/main/java/com/lasgis/arduino/eeprom/memory/RomARRAY.java
+++ java/EepromData/src/main/java/com/lasgis/arduino/eeprom/memory/RomARRAY.java
@@ -8,15 +8,25 @@
 
 package com.lasgis.arduino.eeprom.memory;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 
 /**
  * Массив простых элементов или объектов.
+ * Массив должен состоять из одинаковых элементов,
+ * поэтому нет смысла структуру элемента описывать в каждом элементе.
+ * Однако в начале массива мы должны указать количество элементов массива.
+ *
+ * В EEPROM храним количество элементов массива и описание структуры одного элемента.
  *
  * @author Vladimir Laskin
  * @since 17.05.2018
  */
+@Data
+@EqualsAndHashCode(callSuper = true)
 public class RomARRAY extends RomData {
 
     private ArrayList<RomData> array = new ArrayList<>();
--- java/EepromData/src/main/java/com/lasgis/arduino/eeprom/memory/RomOBJECT.java
+++ java/EepromData/src/main/java/com/lasgis/arduino/eeprom/memory/RomOBJECT.java
@@ -16,8 +16,14 @@ import java.util.ArrayList;
 
 /**
  * Объект структуры или класса.
- * Следующие моменты:
- * 1 -
+ * Структура имеет разные элементы.
+ * Если класс состоит из простых элементов, то размер класса равен размеру в EEPROM
+ * Если класс содержит строки или другие структуры, то при десериализации надо
+ * создавать в памяти объект строки, а в класс ложить указатель.
+ *
+ * Поэтому в EEPROM надо хранить как размер объекта, так и описание его структуры.
+ * Размер объекта вычисляется при создании EEPROM образа.
+ * Описание структуры определяется по элементам объекта.
  *
  * @author Vladimir Laskin
  * @since 17.05.2018
@@ -26,7 +32,9 @@ import java.util.ArrayList;
 @EqualsAndHashCode(callSuper = true)
 public class RomOBJECT extends RomData {
 
+    /** массив, содержащий элементы объекта */
     private ArrayList<RomData> array = new ArrayList<>();
+//    private short size;
 
     public RomOBJECT(final String name) {
         super(name);
