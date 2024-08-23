de395da30a94ada7794cb396297f1dcd13d1b8a2
Vladimir Laskin <laskin@yandex.ru>
2024-08-23 18:55:02 +0600

    java add EditFont

---
+++ java/EditFont/src/main/java/com/lasgis/arduino/editfont/EditFontApplication.java
@@ -0,0 +1,13 @@
+package com.lasgis.arduino.editfont;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EditFontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EditFontApplication.class, args);
+    }
+
+}
---
+++ java/EditFont/src/test/java/com/lasgis/arduino/editfont/EditFontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lasgis.arduino.editfont;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EditFontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
