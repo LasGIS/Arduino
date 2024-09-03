26b649fdf88a2ce657def0407fc4597f7aa472fd
Vladimir Laskin <vlaskin@it-one.ru>
2024-09-04 00:14:43 +0600

    java: EditFont: HtmlDemo +

---
+++ java/EditFont/src/main/java/components/HtmlDemo.java
@@ -0,0 +1,131 @@
+/*
+ *  @(#)HtmlDemo.java  last: 04.09.2024
+ *
+ * Title: LG Java for Arduino
+ * Description: Program for support Arduino.
+ * Copyright (c) 2024, LasGIS Company. All Rights Reserved.
+ */
+
+package components;
+
+/* HtmlDemo.java needs no other files. */
+
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+
+public class HtmlDemo extends JPanel
+    implements ActionListener {
+    JLabel theLabel;
+    JTextArea htmlTextArea;
+
+    public HtmlDemo() {
+        setLayout(new BoxLayout(this, BoxLayout.LINE_AXIS));
+
+        String initialText = "<html>\n" +
+            "Color and font test:\n" +
+            "<ul>\n" +
+            "<li><font color=red>red</font>\n" +
+            "<li><font color=blue>blue</font>\n" +
+            "<li><font color=green>green</font>\n" +
+            "<li><font size=-2>small</font>\n" +
+            "<li><font size=+2>large</font>\n" +
+            "<li><i>italic</i>\n" +
+            "<li><b>bold</b>\n" +
+            "</ul>\n";
+
+        htmlTextArea = new JTextArea(10, 20);
+        htmlTextArea.setText(initialText);
+        JScrollPane scrollPane = new JScrollPane(htmlTextArea);
+
+        JButton changeTheLabel = new JButton("Change the label");
+        changeTheLabel.setMnemonic(KeyEvent.VK_C);
+        changeTheLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        changeTheLabel.addActionListener(this);
+
+        theLabel = new JLabel(initialText) {
+            public Dimension getPreferredSize() {
+                return new Dimension(200, 200);
+            }
+
+            public Dimension getMinimumSize() {
+                return new Dimension(200, 200);
+            }
+
+            public Dimension getMaximumSize() {
+                return new Dimension(200, 200);
+            }
+        };
+        theLabel.setVerticalAlignment(SwingConstants.CENTER);
+        theLabel.setHorizontalAlignment(SwingConstants.CENTER);
+
+        JPanel leftPanel = new JPanel();
+        leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.PAGE_AXIS));
+        leftPanel.setBorder(BorderFactory.createCompoundBorder(
+            BorderFactory.createTitledBorder(
+                "Edit the HTML, then click the button"),
+            BorderFactory.createEmptyBorder(10, 10, 10, 10)));
+        leftPanel.add(scrollPane);
+        leftPanel.add(Box.createRigidArea(new Dimension(0, 10)));
+        leftPanel.add(changeTheLabel);
+
+        JPanel rightPanel = new JPanel();
+        rightPanel.setLayout(new BoxLayout(rightPanel, BoxLayout.PAGE_AXIS));
+        rightPanel.setBorder(BorderFactory.createCompoundBorder(
+            BorderFactory.createTitledBorder("A label with HTML"),
+            BorderFactory.createEmptyBorder(10, 10, 10, 10)));
+        rightPanel.add(theLabel);
+
+        setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        add(leftPanel);
+        add(Box.createRigidArea(new Dimension(10, 0)));
+        add(rightPanel);
+    }
+
+    /**
+     * Create the GUI and show it.  For thread safety, this method should be invoked from the event dispatch thread.
+     */
+    private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("HtmlDemo");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Add content to the window.
+        frame.add(new HtmlDemo());
+
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        //Schedule a job for the event dispatch thread:
+        //creating and showing this application's GUI.
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                //Turn off metal's use of bold fonts
+                UIManager.put("swing.boldMetal", Boolean.FALSE);
+                createAndShowGUI();
+            }
+        });
+    }
+
+    //React to the user pushing the Change button.
+    public void actionPerformed(ActionEvent e) {
+        theLabel.setText(htmlTextArea.getText());
+    }
+}
